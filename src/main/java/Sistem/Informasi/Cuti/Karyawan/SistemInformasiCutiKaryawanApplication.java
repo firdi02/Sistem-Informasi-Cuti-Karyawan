ed299ecb71eef8505e50a2e9c7c3e5d2b1b99b9c
rahmadanil18 <rohmadanil73@gmail.com>
2021-08-13 15:08:45 -0700

    Memulai Project Uas Java

---
+++ src/main/java/Sistem/Informasi/Cuti/Karyawan/SistemInformasiCutiKaryawanApplication.java
@@ -0,0 +1,13 @@
+package Sistem.Informasi.Cuti.Karyawan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemInformasiCutiKaryawanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemInformasiCutiKaryawanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Sistem/Informasi/Cuti/Karyawan/SistemInformasiCutiKaryawanApplicationTests.java
@@ -0,0 +1,13 @@
+package Sistem.Informasi.Cuti.Karyawan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemInformasiCutiKaryawanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
