6bb1eeb916aa466fc04a991ab31c8ec1d359d8f7
rahmadanil18 <rohmadanil73@gmail.com>
2021-08-30 10:29:51 -0700

    menambahkan service export pdf

--- src/main/java/Sistem/Informasi/Cuti/Karyawan/Controller/KaryawanController.java
+++ src/main/java/Sistem/Informasi/Cuti/Karyawan/Controller/KaryawanController.java
@@ -9,8 +9,10 @@ import Sistem.Informasi.Cuti.Karyawan.Model.Repo.DetailCutiRepo;
 import Sistem.Informasi.Cuti.Karyawan.Model.Repo.EmployeeRepo;
 import Sistem.Informasi.Cuti.Karyawan.Model.Repo.HakCutiRepo;
 import Sistem.Informasi.Cuti.Karyawan.Model.Repo.PengajuanCutiRepo;
+import Sistem.Informasi.Cuti.Karyawan.Services.PDF.ExportPdf;
 import Sistem.Informasi.Cuti.Karyawan.Services.PengajuanCutiService;
 import Sistem.Informasi.Cuti.Karyawan.Services.UserAktif;
+import com.lowagie.text.DocumentException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,6 +21,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -109,5 +116,23 @@ public class KaryawanController {
         model.addAttribute("detail",detailCuti);
         return "form_detail_cuti";
     }
+
+    @GetMapping("/cetak/{id}")
+    public void ExsportPdf(HttpServletResponse response, @PathVariable("id") Integer id) throws DocumentException, IOException {
+        response.setContentType("application/pdf");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        String currentDateTime = dateFormatter.format(new Date());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=pengajuan_" + currentDateTime + ".pdf";
+        response.setHeader(headerKey, headerValue);
+
+        DetailCuti detailCutis = detailCutiRepo.findById(id).get();
+
+        ExportPdf exporter = new ExportPdf(detailCutis);
+        exporter.export(response);
+
+    }
+
 
 }
